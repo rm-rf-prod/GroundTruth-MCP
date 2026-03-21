8f4cbe724c665bacb7b65945b803ca4a4fb88d45
Senorit Studio <kontakt@senorit.de>
2026-03-21 13:37:43 +0100

    2.5.1

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SERVER_NAME = "GroundTruth";
-export const SERVER_VERSION = "2.5.0";
+export const SERVER_VERSION = "2.5.1";
 
 // Known size of the full private registry (updated with each release that adds entries)
 export const REGISTRY_BADGE_SIZE = 363;
