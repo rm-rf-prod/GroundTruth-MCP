baa641fe5a6d5daed1047072addd1a5f36d5e9ab
Senorit Studio <94042584+rm-rf-prod@users.noreply.github.com>
2026-08-05 16:04:29 +0200

    7.5.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { config } from "./config.js";
 
 export const SERVER_NAME = "GroundTruth";
-export const SERVER_VERSION = "7.4.1";
+export const SERVER_VERSION = "7.5.0";
 
 // Known size of the full private registry (updated with each release that adds entries)
 export const REGISTRY_BADGE_SIZE = 598;
